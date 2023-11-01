efb43cc56cf7d122d72962e1ba764be99df9ee0d
Tim Fritzsche <tim_fritzsche@outlook.de>
2023-11-01 13:15:40 +0100

    Official Project Restart

--- dashboard/urls.py
+++
@@ -1,6 +0,0 @@
-from .views import BaseDashboard
-from django.urls import path
-
-urlpatterns = [
-    path('', BaseDashboard.as_view(), name='base'),
-    ]
--- static/js/components/hello-world.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('my-test-id').onclick = function() {
-}
-    alert('Hello World!');
